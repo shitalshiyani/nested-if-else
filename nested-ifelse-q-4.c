efe5c256577133478c3b4332491827bcd33a9732
shitalshiyani <127853794+shitalshiyani@users.noreply.github.com>
2023-03-20 22:13:23 +0530

    Add files via upload

---
+++ nested-ifelse-q-4.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b,c,d,e;
+	printf("Enter Value of A=");
+	scanf("%d",&a);
+	printf("Enter Value of B=");
+	scanf("%d",&b);
+	printf("Enter Value of C=");
+	scanf("%d",&c);
+	printf("Enter Value of D=");
+	scanf("%d",&d);
+	printf("Enter Value of E=");
+	scanf("%d",&e);
+	
+	if(a<b){
+		if(a<c){
+			if(a<d){
+				if(a<e){
+					printf("A is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}
+			else{
+				if(d<e){
+					printf("D is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}
+		}
+		else{
+			if(c<d){
+				if(c<e){
+					printf("C is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}
+			else{
+				if(d<e){
+					printf("D is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}
+		}
+	}
+	else{
+		if(b<c){
+			if(b<d){
+				if(b<e){
+					printf("B is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}
+			else{
+				if(d<e){
+					printf("D is minimum");
+				}	
+				else{
+					printf("E is minimum");
+				}
+			}
+		}
+		else{
+			if(c<d){
+				if(c<e){
+					printf("C is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}	
+			else{
+				if(d<e){
+					printf("D is minimum");
+				}
+				else{
+					printf("E is minimum");
+				}
+			}
+		}
+	}
+}
